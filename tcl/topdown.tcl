8a0152d2d0fdf070a5309df5e5138ff3aa40cfc1
Jamey Hicks <jamey.hicks@qrclab.com>
2014-07-24 22:09:04 -0400

    do topdown build if no floorplan specified

--- tcl/topdown.tcl
+++ tcl/topdown.tcl
@@ -46,12 +46,17 @@ set errorlog "Impl/TopDown/critical"
 set commandfilehandle [open "$commandlog.log" w]
 set errorfilehandle [open "$errorlog.log" w]
 
+set impl_start_time [clock seconds]
+
 set dcp_name "./Synth/$module/$module-synth.dcp"
 log_command "read_checkpoint $dcp_name" "$outputDir/[file tail $dcp_name].log"
 foreach dcp $env(MODULE_NETLISTS) {
     log_command "read_checkpoint $dcp" "$outputDir/[file tail $dcp].log"
 }
 foreach xdc $env(XDC) {
+    log_command "read_xdc $xdc" "$outputDir/[file tail $xdc].log"
+}
+foreach xdc $env(FLOORPLAN) {
     log_command "read_xdc $xdc" "$outputDir/[file tail $xdc].log"
 }
 
@@ -66,4 +71,21 @@ log_command place_design    $outputDir/place_design.log
 log_command "write_checkpoint -force $outputDir/$instance-post-place.dcp" $outputDir/temp.log
 report_utilization -file $outputDir/$instance-post-place-util.rpt
 report_timing_summary -file $outputDir/$instance-post-place-timing-summary.rpt
+report_io -file $outputDir/$instance-post-place-io.rpt > hw/temp.log
 
+if {"$env(FLOORPLAN)" == ""} {
+    # just do top down build
+    log_command "phys_opt_design" $outputDir/phys-opt-design.log
+    log_command "write_checkpoint -force $outputDir/$instance-post-phys-opt.dcp" $outputDir/temp.log
+    log_command "route_design" $outputDir/route-design.log
+    report_utilization -file $outputDir/$instance-post-route-util.rpt
+    report_timing_summary -file $outputDir/$instance-post-route-timing-summary.rpt
+    report_timing -sort_by group -max_paths 100 -path_type summary -file $outputDir/$instance-post-route-timing.rpt > hw/temp.log
+    report_io -file $outputDir/$instance-post-route-io.rpt > hw/temp.log
+    report_datasheet -file $outputDir/$instance-post-route_datasheet.rpt > hw/temp.log
+    if {[info exists env(BITFILE)] && $env(BITFILE) != ""} {
+	log_command "write_bitstream -bin_file -force $env(BITFILE)" $outputDir/write_bitstream.log
+    }
+}
+set impl_end_time [clock seconds]
+puts "topdown.tcl elapsed time [expr $impl_end_time - $impl_start_time] seconds"
