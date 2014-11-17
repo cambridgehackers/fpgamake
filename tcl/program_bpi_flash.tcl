38cbc3bd0b66165be9942126876044395eeb81df
Jamey Hicks <jamey.hicks@qrclab.com>
2014-11-17 16:03:50 -0500

    added program_bpi_flash.tcl

---
+++ tcl/program_bpi_flash.tcl
@@ -0,0 +1,64 @@
+
+## Copyright (c) 2014 Quanta Research Cambridge, Inc.
+
+## Permission is hereby granted, free of charge, to any person
+## obtaining a copy of this software and associated documentation
+## files (the "Software"), to deal in the Software without
+## restriction, including without limitation the rights to use, copy,
+## modify, merge, publish, distribute, sublicense, and/or sell copies
+## of the Software, and to permit persons to whom the Software is
+## furnished to do so, subject to the following conditions:
+
+## The above copyright notice and this permission notice shall be
+## included in all copies or substantial portions of the Software.
+
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+## EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+## MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+## NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+## BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+## ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+## CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+## SOFTWARE.
+
+if [file exists {board.tcl}] {
+    source {board.tcl}
+} elseif [info exists env(FPGAMAKE_PARTNAME)] {
+    set partname $env(FPGAMAKE_PARTNAME)
+} else {
+    set boardname vc707
+    set partname {xc7vx485tffg1761-2}
+}
+
+write_cfgmem -force -format MCS -size 128 -interface BPIx16 -loadbit "up 0x0 ./hw/mkTop.bit" mkTop.mcs
+open_hw
+connect_hw_server
+current_hw_target [lindex [get_hw_targets] 0]
+set_property PARAM.FREQUENCY 15000000 [lindex [get_hw_targets] 0]
+open_hw_target
+current_hw_device [lindex [get_hw_devices] 0]
+refresh_hw_device -update_hw_probes false [lindex [get_hw_devices] 0]
+
+create_hw_cfgmem -hw_device [lindex [get_hw_devices] 0] -mem_dev [lindex [get_cfgmem_parts {28f00ap30t-bpi-x16}] 0]
+
+set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
+set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
+set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
+set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
+set_property PROGRAM.ADDRESS_RANGE  {use_file} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
+
+set_property PROGRAM.FILE_1 {./mkTop.mcs} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0]]
+
+set_property PROGRAM.BPI_RS_PINS {none} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
+
+report_property [lindex [get_hw_devices] 0]
+report_property [get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
+report_property [get_property CFGMEM_PART [get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]]
+
+if {![string equal [get_property PROGRAM.HW_CFGMEM_TYPE [lindex [get_hw_devices] 0]] [get_property MEM_TYPE [get_property CFGMEM_PART [get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]]]] } {
+    create_hw_bitstream -hw_device [lindex [get_hw_devices] 0] [get_property PROGRAM.HW_CFGMEM_BITFILE [ lindex [get_hw_devices] 0]];
+    program_hw_devices [lindex [get_hw_devices] 0];
+};
+
+program_hw_cfgmem -hw_cfgmem [get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
+
