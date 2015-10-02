122d241da338879de5756f3faa58d9e62e128d5d
Han Wang <hwang@cs.cornell.edu>
2015-10-02 11:28:54 -0400

    added script to generate from altera qsys

--- tcl/ipcore.tcl
+++ tcl/ipcore.tcl
@@ -156,3 +156,27 @@ proc fpgamake_altera_qmegawiz {ip_path ip_name} {
 			$ipdir/$boardname/synthesis/$ip_name/$ip_name.v
 	}
 }
+
+proc fpgamake_altera_ipcore_qsys {core_name core_version ip_name} {
+    global ipdir boardname partname
+
+    exec -ignorestderr -- ip-generate \
+            --project-directory=$ipdir/$boardname                            \
+            --output-directory=$ipdir/$boardname/synthesis/$ip_name          \
+            --file-set=QUARTUS_SYNTH                                         \
+            --report-file=html:$ipdir/$boardname/$ip_name.html               \
+            --report-file=sopcinfo:$ipdir/$boardname/$ip_name.sopcinfo       \
+            --report-file=cmp:$ipdir/$boardname/$ip_name.cmp                 \
+            --report-file=qip:$ipdir/$boardname/synthesis/$ip_name/$ip_name.qip       \
+            --report-file=svd:$ipdir/$boardname/synthesis/$ip_name/$ip_name.svd       \
+            --report-file=regmap:$ipdir/$boardname/synthesis/$ip_name/$ip_name.regmap \
+            --report-file=xml:$ipdir/$boardname/$ip_name.xml                 \
+            --system-info=DEVICE_FAMILY=StratixV                             \
+            --system-info=DEVICE=$partname                                   \
+            --system-info=DEVICE_SPEEDGRADE=2_H2                             \
+            --language=VERILOG                                               \
+            --component-file=$core_name                                      \
+            --output-name=$ip_name
+}
+
+
